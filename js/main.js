28d8dfb41d3f7a5c73a133b6e86b93a74d1e6edc
mwhelehan <matthew.whelehan@gmail.com>
2022-09-06 08:06:19 -0400

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// document.getElementById("count-el").innerText=5
+
+// let count = 5 +7
+
+// console.log(count)
+
+// let Myage = 47
+// console.log(Myage)
+
+
+let myage = 47
+
+let humandogratio = 7
+let mydogage = myage * humandogratio
+console.log (mydogage)
