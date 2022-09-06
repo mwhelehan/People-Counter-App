dd8bbfa9b7022ae92c66dab9939d78f184edaf4b
mwhelehan <matthew.whelehan@gmail.com>
2022-09-06 13:41:55 -0400

    save

--- js/main.js
+++ js/main.js
@@ -4,22 +4,62 @@
 
 // console.log(count)
 
-// let Myage = 47
-// console.log(Myage)
+// INITIALIZE the count as zero
+// listen for clicks on button
+// increment the count variable when thr button is clicked
+// change the count-el in the html to refect the new count
+
+// function increment() {
+// console.log("the button was clicked")
+
+// }
+// increment()
 
 
-// let myage = 47
+// function fortytwo(){
+// console.log ("42")
 
-// let humandogratio = 7
-// let mydogage = myage * humandogratio
-// console.log (mydogage)
+// }
 
-let bonuspoints = 50
-bonuspoints = bonuspoints +50
-console.log(bonuspoints)
+// fortytwo ()
 
-bonuspoints = bonuspoints - 75
-console.log(bonuspoints)
 
-bonuspoints = bonuspoints + 45
-console.log(bonuspoints)
+// let lap1 = 34
+// let lap2 = 33
+// let lap3 = 36
+
+// function add_laps(){
+// let totaltime = lap1 + lap2 +lap3
+// console.log (totaltime)
+
+// }
+// add_laps()
+
+
+// let lapscompleted = 0
+
+// create a function that increments the laps completed variable with one
+//run three times
+
+// function incrementlap(){
+// lapscompleted = lapscompleted + 1
+// console.log(lapscompleted)
+// }
+
+// incrementlap()
+// incrementlap()
+// incrementlap()
+
+// INITIALIZE the count as zero
+// listen for clicks on button
+// increment the count variable when thr button is clicked
+// change the count-el in the html to refect the new count
+
+// let count = 0
+// console.log (count)
+
+function increment(){
+console.log ("clicked")
+
+}
+increment()
